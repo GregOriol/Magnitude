0b1cd5aa32e8312717eb15805a3a63d63adf3c63
Idris Khenchil <Idris@idriss-macbook-pro.local>
2015-11-01 15:18:13 -0500

    Added quote & author onto canvas

--- Magnitude/Magnitude.swift
+++ Magnitude/Magnitude.swift
@@ -8,6 +8,34 @@
 
 import ScreenSaver
 
-class Magnitude: ScreenSaverView {
-    
+public class Magnitude: ScreenSaverView {
+    public override func drawRect(rect: NSRect) {
+        let quote = "Great companies are built on great products." as NSString
+        let author = "Elon Musk"
+        
+        let attributes = [
+            NSForegroundColorAttributeName: NSColor.blackColor()
+        ]
+        
+        let quoteSize = quote.sizeWithAttributes(attributes)
+        let authorSize = author.sizeWithAttributes(attributes)
+        
+        let authorRect = CGRect(
+            x: round((bounds.width - authorSize.width) / 2),
+            y: round(((bounds.height - authorSize.height) / 2) - 20),
+            width: authorSize.width,
+            height: authorSize.height
+        )
+        
+        let quoteRect = CGRect(
+            x: round((bounds.width - quoteSize.width) / 2),
+            y: round((bounds.height - quoteSize.height) / 2),
+            width: quoteSize.width,
+            height: quoteSize.height
+        )
+        
+        //Draw text on canvas
+        quote.drawInRect(quoteRect, withAttributes: attributes)
+        author.drawInRect(authorRect, withAttributes: attributes)
+    }
 }
